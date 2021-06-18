aa49ad44910acaf5a00d74865a18c3b83ca1804c
Alex Dominguez <alexdominguezwebdev@gmail.com>
2021-06-18 07:16:46 -0700

    Two radio buttons asking user for learning type/problem approach

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
     $("input[type='button']").click(function() {
         var educationValue = $("input[name='education']:checked").val();
+        var problemValue = $("input[name='problem']:checked").val();
+        var learnValue = $("input[name='learn']:checked").val();
         if (educationValue) {
             alert("Your educational level is - " + educationValue);
         }
+        if (problemValue) {
+            alert("Your approach to solving problems is " + problemValue);
+        }
+        if (learnValue) {
+            alert(" You learn the best by" + learnValue + "process");
+        }
+        //$("label").toggle();
+        //$("label").show();
+
+
     });
 });

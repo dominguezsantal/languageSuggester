244947c44c5da96e9206bb70d15871ed1a563f43
Alex Dominguez <alexdominguezwebdev@gmail.com>
2021-06-18 12:51:30 -0700

    simplify var  names in jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,25 @@
 $(document).ready(function() {
     $("input[type='button']").click(function() {
-        var educationValue = $("input[name='education']:checked").val();
-        var problemValue = $("input[name='problem']:checked").val();
-        var learnValue = $("input[name='learn']:checked").val();
-        var personalityValue = $("input[name= 'personality']:checked").val();
-        var projectValue = $("input[name='projectChoice']:checked").val();
+        const question1 = $("input[name='education']:checked").val();
+        const question2 = $("input[name='problem']:checked").val();
+        const question3 = $("input[name='learn']:checked").val();
+        const question4 = $("input[name= 'personality']:checked").val();
+        const question5 = $("input[name='projectChoice']:checked").val();
 
-        if (educationValue) {
-            alert("Your educational level is - " + educationValue);
+        if (question1) {
+            alert("Your educational level is - " + question1);
         }
-        if (problemValue) {
-            alert("Your approach to solving problems is " + " " + problemValue);
+        if (question2) {
+            alert("Your approach to solving problems is " + " " + question2);
         }
-        if (learnValue) {
-            alert(" You learn the best by" + " " + learnValue + " " + "process");
+        if (question3) {
+            alert(" You learn the best by" + " " + question3 + " " + "process");
         }
-        if (personalityValue) {
-            alert("Your personality type is:" + personalityValue);
+        if (question4) {
+            alert("Your personality type is:" + question4);
         }
-        if (projectValue) {
-            alert("Your choice for your project is:" + "  " + projectValue);
+        if (question5) {
+            alert("Your choice for your project is:" + "  " + question5);
         }
 
 
